d6060b54d5bd165f2837c9d922c234da5f522f28
Luis Loaeza <luis.loaeza@hotmail.com>
2021-07-26 00:20:05 -0500

    Added user registration link

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from dashboard.models import StudentDetails
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
 
 
 @login_required
@@ -10,21 +12,52 @@ def students(request):
                   {'StudentDetails': StudentDetails.objects.all()})
 
 
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user:
-            login(request, user)
-            return redirect('students')
-        else:
-            return render(request, 'dashboard/home.html', {'error': 'Invalid user and/or password'})
-    return render(request, 'dashboard/home.html',
-                  {'StudentDetails': StudentDetails.objects.all()})
+    if request.user.is_authenticated:
+        return redirect('students')
+    else:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('students')
+            else:
+                return render(request, 'dashboard/home.html', {'error': 'Invalid user and/or password'})
+        return render(request, 'dashboard/home.html',
+                      {'StudentDetails': StudentDetails.objects.all()})
 
 
 @login_required
 def evaluations(request):
     return render(request, 'dashboard/evaluations.html',
                   {'StudentDetails': StudentDetails.objects.all()})
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+
+        if password != password_confirmation:
+            return render(request, 'dashboard/signup.html', {'error': 'The passwords entered do not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'dashboard/signup.html', {'error': 'The username is already taken'})
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        return redirect('login')
+    return render(request, 'dashboard/signup.html')
--- student_dashboard/urls.py
+++ student_dashboard/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from dashboard.views import login_view
+from dashboard.views import login_view, logout_view, signup_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('dashboard/', include('dashboard.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
