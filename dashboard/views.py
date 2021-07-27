d1f36027f68f273f1393fab333147d99d8235e60
Luis Loaeza <luis.loaeza@hotmail.com>
2021-07-27 00:44:56 -0500

    Change signup to django form

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,35 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from dashboard.models import StudentDetails
 from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegisterForm(UserCreationForm):
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('Email Already exists')
+        return email
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'dashboard/signup.html', {'form': form})
 
 
 @login_required
--- student_dashboard/urls.py
+++ student_dashboard/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from dashboard.views import login_view, logout_view, signup_view
+from dashboard.views import login_view, logout_view, signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', signup_view, name='signup'),
+    path('signup/', signup, name='signup'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('dashboard/', include('dashboard.urls'))
